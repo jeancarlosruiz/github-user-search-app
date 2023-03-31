db12397e6a81341b967e2d93c67fc8f3eadd64d3
Jean Carlos Ruiz <66891780+jeancarlosruiz@users.noreply.github.com>
2023-03-31 13:00:10 -0400

    Project init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import htmlPurge from "vite-plugin-html-purgecss";
+import htmlPurge from 'vite-plugin-html-purgecss';
 
 export default {
   plugins: [htmlPurge()],
+  base: '/github-user-search-app/',
 };
 
 // npm i vite-plugin-html-purgecss -D
